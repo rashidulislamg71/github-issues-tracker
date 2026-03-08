05febef02f47c83f88e174d679fe370d7753617c
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-03-08 08:54:14 +0600

    Aperture img fix

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const buttons = document.querySelectorAll(".primaryBtn");
+// button active function
+const btnActiveSystem = () => {
+  const buttons = document.querySelectorAll(".primaryBtn");
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    // remove class
-    buttons.forEach((b) => {
-      b.classList.remove("bg-blue-700", "text-white");
-      b.classList.add("bg-white");
-      
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // remove class
+      buttons.forEach((b) => {
+        b.classList.remove("bg-blue-700", "text-white");
+        b.classList.add("bg-white");
+      });
+      //add class
+      btn.classList.remove("bg-white");
+      btn.classList.add("bg-blue-700", "text-white");
+      console.log(btn);
     });
-
-    //add class
-    btn.classList.remove("bg-white");
-    btn.classList.add("bg-blue-700", "text-white");
-    console.log(btn)
   });
+};
 
-});
-
+document.addEventListener("DOMContentLoaded", btnActiveSystem);
